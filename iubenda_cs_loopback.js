c5723d17c7be7b67aa7726404b561a02b36eea42
Ahmed Abdelfatah <giniyat202@yahoo.com>
2022-06-20 10:19:29 +0200

    Update iubenda_cs_loopback.js

--- iubenda_cs_loopback.js
+++ iubenda_cs_loopback.js
@@ -1,6 +1,6 @@
 (function() {
   var script = document.createElement('script');
-  script.src = window.__csUrl || 'http://localhost:3012/cookie_solution/iubenda_cs.js';
+  script.src = window.__csUrl || '/cookie_solution/iubenda_cs.js';
   script.async = true;
   document.head.appendChild(script);
 })();
